ff1ae182bb3ce170532af5e621c5b9f9a6e514a5
RayyanAhmed76 <rayyanahmedrakk@gmail.com>
2025-08-21 23:50:11 +0500

    created a real-time device tracker

---
+++ app.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const server = http.createServer(app);
+const socket = require("socket.io");
+
+const io = socket(server);
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  socket.on("send-location", (data) => {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+
+  socket.on("disconnect", () => {
+    socket.emit("user-disconnected", socket.id);
+  });
+});
+
+app.get("/", async (req, res) => {
+  res.render("index");
+});
+
+server.listen(9000);
